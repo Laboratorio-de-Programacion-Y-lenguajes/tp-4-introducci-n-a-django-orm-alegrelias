4f8fc3fdbbc48ee3b6ce162c4bc217ed3a4d799f
alegrelias26 <elias.n.alegre26-1@gmail.com>
2026-05-05 12:49:19 -0300

    Se corrige el orden de __future__ y se importa directamente F en lugar de importar models por separado

--- catalogo/queries.py
+++ catalogo/queries.py
@@ -1,8 +1,6 @@
-import models
-
 from __future__ import annotations
 
-from django.db.models import Count, Q
+from django.db.models import Count, F, Q
 
 from .models import Autor, Libro
 
@@ -18,22 +16,8 @@ def autores_con_mas_de_n_libros(n: int):
 def libros_sin_disponibilidad():
     return Libro.objects.annotate(
             activos=Count("prestamo", filter=Q(prestamo__fecha_devolucion__isnull=True))
-        ).filter(activos__gte=models.F("cantidad_total"))
+        ).filter(activos__gte=F("cantidad_total"))
 
 
 def top_n_libros_mas_prestados(n: int):
-    """
-    Devuelve los N libros con más préstamos (en total, sin importar si están activos).
-
-    Args:
-        n: cantidad de libros a retornar
-
-    Returns:
-        QuerySet[Libro] con hasta n elementos, ordenados de más a menos prestados.
-
-    Pista:
-        Libro.objects.annotate(total_prestamos=Count("prestamo"))
-                    .order_by("-total_prestamos")[:n]
-    """
-    # TODO: implementar con annotate + order_by + slicing
-    raise NotImplementedError
+    return Libro.objects.annotate(total_prestamos=Count("prestamo")).order_by("-total_prestamos", "titulo")[:n]
